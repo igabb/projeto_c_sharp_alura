a512a4227b4a595b25d830e535efa57852f393e0
Gabriel Silva <gb0104@hotmail.com>
2024-07-09 18:12:57 -0300

    atualizando o projeto

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -4,15 +4,67 @@
     {
         static void Main(string[] args)
         {
-            /*// Screen Sound
-            string mensagemDeBoasVindas = "Boas vindas ao Screen Saound";
-            Console.WriteLine(mensagemDeBoasVindas)*/
+            // Screen Sound
+            string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 
-            string curso = "C#";
-            string nome = "Gabriel";
-            string sobrenome = "Silva";
+            void ExibirMensagemDeBoasVindas()
+            {
+                Console.WriteLine
+                (@"
+                    
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+                ");
+                Console.WriteLine(mensagemDeBoasVindas);
+            }
 
-            Console.WriteLine(curso + " " + nome + " " + sobrenome);
+            void ExibirOpcoesDoMenu()
+            {
+                Console.WriteLine("\nDigite 1 para registrar uma banda");
+                Console.WriteLine("Digite 2 para mostrar todas as bandas");
+                Console.WriteLine("Digite 3 para avaliar uma banda");
+                Console.WriteLine("Digite 4 para exibir a média de uma banda");
+                Console.WriteLine("Digite -1 para sair");
+
+                Console.Write("\nDigite a sua opção: ");
+                int opcaoEscolhida = int.Parse(Console.ReadLine()!);
+
+                switch (opcaoEscolhida)
+                {
+                    case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+                        break;
+                    case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+                        break;
+                    case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+                        break;
+                    case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+                        break;
+                    case -1: Console.WriteLine("Tchau tchau");
+                        break;
+                    default: Console.WriteLine("Opção inválida");
+                        break;
+                }
+
+            }
+
+            ExibirMensagemDeBoasVindas();
+            ExibirOpcoesDoMenu();
+
+
+
+           /*
+                Exercício 1
+           
+                string curso = "C#";
+                string nome = "Gabriel";
+                string sobrenome = "Silva";
+
+                Console.WriteLine(curso + " " + nome + " " + sobrenome); 
+           */
 
 
         }
--- PrimeiroProjeto/obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
+++ PrimeiroProjeto/obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c3e1768e771e5f72a3392fb2d529a44a4f87e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
