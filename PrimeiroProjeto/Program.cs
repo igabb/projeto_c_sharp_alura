de96ac32186b8665ea12a1805900b1f6045b5f63
Gabriel Silva <gb0104@hotmail.com>
2024-07-09 17:06:14 -0300

    Inicio do projeto

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*// Screen Sound
+            string mensagemDeBoasVindas = "Boas vindas ao Screen Saound";
+            Console.WriteLine(mensagemDeBoasVindas)*/
+
+            string curso = "C#";
+            string nome = "Gabriel";
+            string sobrenome = "Silva";
+
+            Console.WriteLine(curso + " " + nome + " " + sobrenome);
+
+
+        }
+    }
+}
